c2ffa2fdef2db11cec371bf8ae87f1937ee1c163
Stephen Li <stephenli.ca@gmail.com>
2014-09-07 15:31:55 -0400

    Refactored scraper
    - Now queues up listing page and screenshot pages separately to allow
      greater rate control via queue.yaml

--- scraper.py
+++ scraper.py
@@ -3,9 +3,9 @@ import webapp2
 import logging
 import json
 import urlparse
+import time
 from datetime import timedelta
 from datetime import datetime
-
 from google.appengine.ext import db
 from google.appengine.api import urlfetch
 from google.appengine.api import taskqueue
@@ -19,12 +19,17 @@ from models.screenshot import Screenshot
 import settings
 
 #------------------------------------------------------------------------------
-# Steam Scraper
+# Globals
 #------------------------------------------------------------------------------
 
-def request_soup(url):
-    logging.debug('Getting soup of ' + url)
+TEST_USERNAME             = 'trinovantes'
+HEADER_STEAM_USERNAME_KEY = 'steam_username'
+HEADER_NEXT_PAGE_KEY      = 'current_page_url'
 
+listing_queue    = taskqueue.Queue('listing-queue')
+screenshot_queue = taskqueue.Queue('screenshot-queue')
+
+def request_soup(url):
     result = urlfetch.fetch(
         url,
         headers = {'User-Agent': settings.user_agent}
@@ -33,78 +38,111 @@ def request_soup(url):
         return BeautifulSoup(result.content)
     else:
         logging.error('Received ' +  str(result.status_code) + ' error from ' + url)
-        return None      
+        return None  
 
-class SteamScraper:
-    user          = None
-    base_url      = None
-    next_page_url = None
+#------------------------------------------------------------------------------
+# Listing Scraper
+#------------------------------------------------------------------------------
 
-    def __init__(self, user):
-        self.user          = user
-        self.base_url      = settings.steam_base_url + '/id/' + self.user.steam_username + '/screenshots/'
-        self.next_page_url = self.base_url
+def get_base_url(username):
+    return settings.steam_base_url + '/id/' + username + '/screenshots/'    
+
+class ListingScraper:
+    user = None
+
+    def __init__(self, username):
+        self.user = User.all().filter('steam_username =', username).get()
 
     def fix_url(self, url):
-        if 'http' not in url: # when debugging, the urls are relative instead of absolute
-            url = self.base_url + url
+        if 'http' not in url: # When debugging, the urls are relative instead of absolute
+            url = get_base_url(self.user.steam_username) + url
         return url
 
-    def finish_user(self):
-        self.user.last_scraped = datetime.now()
-        self.user.put()
-        # clean up so we don't accidently do anything after user's last_scraped property is updated
-        self.user          = None
-        self.base_url      = None
-        self.next_page_url = None
-
-    def process(self):
-        logging.info('Starting to process \'' + self.user.steam_username + '\' last_scraped:' + self.user.last_scraped.strftime('%Y-%m-%d %H:%M:%S.%f'))
-        while self.next_page_url is not None:
-            self.process_list_page(self.next_page_url)
-
-        self.finish_user()
-
-    def process_list_page(self, list_url):
-        page_soup = request_soup(list_url)
-        if page_soup is None:
-            self.next_page_url = None
+    def run(self, current_page_url):
+        if self.user is None:
+            logging.warning('Attempting to get listing for an empty user')
             return
 
-        screenshot_links = page_soup.find_all('a', class_='userScreenshotLink')
+        logging.info('Processing \'' + current_page_url + '\' steam_username:' + self.user.steam_username + ' last_scraped:' + self.user.last_scraped.strftime('%Y-%m-%d %H:%M:%S.%f'))
+        page_soup = request_soup(current_page_url)
 
-        # get next page's link
+        # Check if we got 200 from Steam
+        if page_soup is None:
+            return
+        
+        # Iterate over this page's screenshots
+        screenshot_links = page_soup.find_all('a', class_='userScreenshotLink')
+        for link in screenshot_links:
+            screenshot_queue.add(taskqueue.Task(
+                url = '/scraper/screenshot',
+                params = {
+                    HEADER_STEAM_USERNAME_KEY:  self.user.steam_username,
+                    HEADER_NEXT_PAGE_KEY:       self.fix_url(link['href'])
+                }
+            ))
+
+        # Queue up next page or set current user as finished
         next_page_arrow = page_soup.find('img', class_='pagingRightArrowImg')
         if next_page_arrow is None:
-            self.next_page_url = None
+            self.user.last_scraped = datetime.now()
+            self.user.put()
+            self.user = None # cleanup
         else:
-            self.next_page_url = self.fix_url(next_page_arrow.parent['href'])
-            
-        # iterate over this page's screenshots
-        for link in screenshot_links: # TODO batch
-            screenshot_url = self.fix_url(link['href'])
-            self.process_screenshot_page(screenshot_url)
+            listing_queue.add(taskqueue.Task(
+                url = '/scraper/listing', 
+                params = {
+                    HEADER_STEAM_USERNAME_KEY:  self.user.steam_username,
+                    HEADER_NEXT_PAGE_KEY:       self.fix_url(next_page_arrow.parent['href'])
+                }
+            ))
 
-    def process_screenshot_page(self, screenshot_url):
-        parsed_url = urlparse.urlparse(screenshot_url)
-        screenshot_id   = urlparse.parse_qs(parsed_url.query)['id'][0]
+class ListingScraperHandler(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get(HEADER_STEAM_USERNAME_KEY)
+        next_page_url = self.request.get(HEADER_NEXT_PAGE_KEY)
 
-        # first check if screenshot exists
+        # If this task is queued up by the scheduler, then use the base url
+        if not next_page_url:
+            next_page_url = get_base_url(username)
+
+        scraper = ListingScraper(username)
+        scraper.run(next_page_url)
+
+#------------------------------------------------------------------------------
+# Screenshot Scraper
+#------------------------------------------------------------------------------
+
+class ScreenshotScraper:
+    user = None
+
+    def __init__(self, username):
+        self.user = User.all().filter('steam_username =', username).get()
+
+    def run(self, screenshot_page_url):
+        if self.user is None:
+            logging.warning('Attempting to get screenshot for an empty user')
+            return
+
+        # First check if screenshot exists in db already
+        parsed_url = urlparse.urlparse(screenshot_page_url)
+        screenshot_id = urlparse.parse_qs(parsed_url.query)['id'][0]
         if Screenshot.all().filter('screenshot_id =', screenshot_id).get() is not None:
             return
 
-        page_soup = request_soup(screenshot_url)
+        # Then check if we got 200 from Steam
+        page_soup = request_soup(screenshot_page_url)
         if page_soup is None:
             return
 
         screenshot_src  = page_soup.find('img', class_='userScreenshotImg')['src']
         screenshot_desc = page_soup.find('a', class_='secondarynav_pageTitleHeader').string.strip()
         screenshot_game = page_soup.find(id='gameName').find('a', class_='itemLink').string.strip()
-#TODO check for spoiler
+        #TODO Check for spoiler tag
+        
         s = Screenshot(
             owner         = self.user,
             screenshot_id = screenshot_id,
-            url           = screenshot_url,
+            url           = screenshot_page_url,
             src           = screenshot_src,
             desc          = screenshot_desc,
             game          = screenshot_game
@@ -115,25 +153,24 @@ class SteamScraper:
         else:
             logging.debug('Successfully saved screenshot for ' + self.user.steam_username + ' id:' + screenshot_id)
 
-#------------------------------------------------------------------------------
-# Google App Engine
-#------------------------------------------------------------------------------
-
-class ScraperTaskHandler(webapp2.RequestHandler):
+class ScreenshotScraperHandler(webapp2.RequestHandler):
     def post(self):
-        steam_username = self.request.get('steam_username')
-        user = User.all().filter('steam_username =', steam_username).get()
-        scraper = SteamScraper(user)
-        scraper.process()
+        username = self.request.get(HEADER_STEAM_USERNAME_KEY)
+        next_page_url = self.request.get(HEADER_NEXT_PAGE_KEY)
+        scraper = ScreenshotScraper(username)
+        scraper.run(next_page_url)
+
+#------------------------------------------------------------------------------
+# Scheduler
+#------------------------------------------------------------------------------
 
 class ScraperSchedulerHandler(webapp2.RequestHandler):
     def get(self):
         if settings.debug:
-            test_user = User.all().filter('steam_username =', 'trinovantes').get()
+            test_user = User.all().filter('steam_username =', TEST_USERNAME).get()
             if test_user is None:
-                test_user = User(steam_username = 'trinovantes')
+                test_user = User(steam_username=TEST_USERNAME)
                 test_user.put()
-
             users = [test_user]
         else:
             more_than_a_day_ago = datetime.now() - timedelta(seconds=-settings.delay_seconds)
@@ -141,14 +178,19 @@ class ScraperSchedulerHandler(webapp2.RequestHandler):
 
         if users is not None:
             logging.info('Scheduling scrapper on ' + str(len(users)) + ' user(s)')
-            queue = taskqueue.Queue('scraper-queue')
             for user in users:
-                queue.add(taskqueue.Task(url='/scraper/run', params={'steam_username': user.steam_username}))
+                listing_queue.add(taskqueue.Task(
+                    url = '/scraper/listing', 
+                    params = {
+                        HEADER_STEAM_USERNAME_KEY: user.steam_username
+                    }
+                ))
 
 
 application = webapp2.WSGIApplication([
-    ('/scraper/run',       ScraperTaskHandler),
-    ('/scraper/scheduler', ScraperSchedulerHandler)
+    ('/scraper/listing',    ListingScraperHandler),
+    ('/scraper/screenshot', ScreenshotScraperHandler),
+    ('/scraper/scheduler',  ScraperSchedulerHandler)
 ], debug = settings.debug)
 
 if __name__ == '__main__':
